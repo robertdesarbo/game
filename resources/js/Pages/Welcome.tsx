afaef321d90de5b001ff4caf902a250d91d7cec3
Robert DeSarbo <robertdesarbo@gmail.com>
2023-12-10 20:37:19 -0500

    Use Laravel Sail

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -20,7 +20,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<
                                 href={route('login')}
                                 className="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500"
                             >
-                                Log in
+                                Log in23412
                             </Link>
 
                             <Link
